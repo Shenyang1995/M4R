a490d3cfd185665f221b96c6f8b1f72f717578a4
shenyang wu <shenyang.wu16@imperial.ac.uk>
2019-11-14 14:08:51 +0000

    1 goal left

--- src/M4R.lean
+++ src/M4R.lean
@@ -1,6 +1,7 @@
 import algebra.group -- for is_add_group_hom
 import group_theory.subgroup -- for kernels
 import algebra.module
+import tactic.linarith
 
 class G_module (G : Type*) [group G] (M : Type*) [add_comm_group M]
   extends  has_scalar G M :=
@@ -11,15 +12,50 @@ class G_module (G : Type*) [group G] (M : Type*) [add_comm_group M]
 def cochain(n:ℕ)(G : Type*) [group G] (M : Type*) [add_comm_group M] [G_module G M] := 
 fin n → G → M
 
-def F(n:ℕ)(j:ℕ ) (G : Type*)[group G](g:fin (n+1)→ G):fin n→ G
-:= λ k,  if k.val < j then g k else 
-         if k.val=j then g k *g (k+1)--⟨k.val+1, add_lt_add_right k.2 1⟩  
-         else g (k+1)
+def F{n:ℕ}(j:ℕ) {G : Type*}[group G](g:fin (n+1)→ G):fin n→ G
+:= λ k,  if k.val < j then g ⟨k.val, lt_trans k.2 $ lt_add_one _⟩ else 
+         if k.val=j then g ⟨k.val, lt_trans k.2 $ lt_add_one _⟩ *g ⟨k.val+1, add_lt_add_right k.2 1⟩  
+         else g  ⟨k.val+1, add_lt_add_right k.2 1⟩  
 
 
+def XYZ (n : ℕ): fin n → fin (n + 1) := coe
+example : XYZ 3 ⟨2,by linarith⟩ = ⟨2, by linarith⟩ := rfl
 
+--set_option pp.all true
 theorem degenerate(n:ℕ)(j:ℕ)(k:ℕ)(G : Type*)[group G](g:fin (n+2)→ G)(h:j≤ k): 
-F n j G (F (n+1) (k+1) G g) = F n k G (F (n+1) j G g):=sorry  
+F j (F (k+1) g) = F k (F j g):=
+begin 
+funext t,
+conv begin
+  to_lhs,
+  simp only [F],
+end,
+split_ifs;try {refl}; try {rw (show (↑t : fin(n + 1)).val = t.val, by refl) at *}; try {exfalso;linarith};
+--rw (show (↑t : fin(n + 1)).val = t.val, by refl) at *,
+--change t.val < k + 1 at h_2,
+unfold F,
+{split_ifs;try {rw (show (↑t : fin(n + 1)).val = t.val, by refl) at *}; try {exfalso;linarith}; try {refl}; try {simp only [mul_assoc]}; try {finish}},
+{split_ifs;try {rw (show (↑t : fin(n + 1)).val = t.val, by refl) at *}; try {exfalso;linarith}; try {refl}; try {simp only [mul_assoc]}; try {finish}},
+{split_ifs;try {rw (show (↑t : fin(n + 1)).val = t.val, by refl) at *}; try {exfalso;linarith}; try {refl}; try {simp only [mul_assoc]}; try {finish}},
+{split_ifs;try {rw (show (↑t : fin(n + 1)).val = t.val, by refl) at *}; try {exfalso;linarith}; try {refl}; try {simp only [mul_assoc]}; try {finish}},
+{split_ifs;try {rw (show (↑t : fin(n + 1)).val = t.val, by refl) at *}; try {exfalso;linarith}; try {refl}; try {simp only [mul_assoc]}; try {finish}},
+swap,
+{split_ifs;try {rw (show (↑t : fin(n + 1)).val = t.val, by refl) at *}; try {exfalso;linarith}; try {refl}; try {simp only [mul_assoc]}; try {finish}},
+
+
+{split_ifs;try {rw (show (↑t : fin(n + 1)).val = t.val, by refl) at *}; try {exfalso;linarith}; try {refl}; try {simp only [mul_assoc]}; try {finish}},
+
+
+
+
+--split_ifs; try {refl}; try {rw (show (t : fin(n + 1)).val = t.val, by refl) at *},
+{split_ifs;try {rw (show (↑t : fin(n + 1)).val = t.val, by refl) at *}; try {exfalso;linarith}; try {refl}; try {simp only [mul_assoc]}; try {finish}},
+
+
+
+
+
+end
 
 #exit
 def d(n:ℕ)(G : Type*) [group G] (M : Type*) [add_comm_group M] [G_module G M]:
