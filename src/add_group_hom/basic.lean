b061374639b0cbf45a261084940762352847c6e3
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2020-03-06 12:00:25 +0000

    More API for bundled subgroups and group homs

--- src/add_group_hom/basic.lean
+++ src/add_group_hom/basic.lean
@@ -6,6 +6,12 @@ import add_subgroup.basic
 @[reducible] def add_group_hom (G : Type*) (H : Type*) [add_group G] [add_group H] :=
   add_monoid_hom G H
 
+def add_group_hom.comp {G : Type*} {H : Type*} {K : Type*}
+  [add_group G] [add_group H] [add_group K]
+    (f : add_group_hom H K) (g : add_group_hom G H): 
+  add_group_hom G K := add_monoid_hom.comp f g
+
+
 def add_group_hom.mk {G : Type*} [add_group G] {H : Type*} [add_group H] (f : G → H)
 (h : ∀ a b : G, f(a + b) = f a + f b) : add_group_hom G H :=
 { to_fun := f,
@@ -21,68 +27,92 @@ namespace add_group_hom
 
 variables {G : Type*} {H : Type*} [add_group G] [add_group H]
 
-def ker (f : add_group_hom G H) : add_subgroup G :=
-{ carrier := {x : G | f x = 0},
+def map {G₁ : Type*} [add_group G₁] {G₂ : Type*} [add_group G₂]
+  (f : G₁ →+ G₂) (H : add_subgroup G₁) : add_subgroup G₂ :=
+{ carrier := (f : G₁ → G₂) '' H.carrier,
   is_add_subgroup := { 
-    zero_mem := f.map_zero,
+    zero_mem := ⟨0, H.zero_mem, f.map_zero⟩,
+    add_mem := begin rintros _ _ ⟨c, hc, rfl⟩ ⟨d, hd, rfl⟩,
+      exact ⟨c + d, H.add_mem hc hd, f.map_add c d⟩ end,
+    neg_mem := begin rintros _ ⟨c, hc, rfl⟩, exact ⟨-c, H.neg_mem hc, f.map_neg c⟩ end
+  } 
+}
+
+def comap {G₁ : Type*} [add_group G₁] {G₂ : Type*} [add_group G₂]
+  (f : G₁ →+ G₂) (H : add_subgroup G₂) : add_subgroup G₁ :=
+{ carrier := (f : G₁ → G₂) ⁻¹' H.carrier,
+  is_add_subgroup := { 
+    zero_mem := begin
+      show f 0 ∈ H,
+      rw f.map_zero,
+      exact H.zero_mem
+    end,
     add_mem := begin
       intros a b ha hb,
-      change f a = 0 at ha,
-      change f b = 0 at hb,
-      show f (a + b) = 0,
-      rw [f.map_add, ha, hb, add_zero],
+      show f (a + b) ∈ H,
+      rw f.map_add,
+      exact H.add_mem ha hb,
     end,
     neg_mem := begin
       intros a ha,
-      change f a = 0 at ha,
-      show f (-a ) = 0,
-      have h2: f (a+(-a))=0,
-      norm_num,
-      --have h3: f(a)+f(-a)=0,
-      rw f.map_add at h2,
-      rw ha at h2,
-      rw zero_add at h2, exact h2,
+      show f (-a) ∈ H,
+      rw f.map_neg,
+      exact H.neg_mem ha
     end
-  }
+  } 
 }
 
-def range (f : add_group_hom G H) : add_subgroup H :=
-{ carrier := set.range f,
-  is_add_subgroup := { 
-    zero_mem := begin
-      use 0,
-      exact f.map_zero
-    end,
-    add_mem := begin
-      intros a b ha hb,
-      cases ha with c hc,
-      cases hb with d hd,
-      use c+d,
-      rw [f.map_add, hc, hd],
-    end,
-    neg_mem := begin
-      intro b,
-      intro hb,
-      cases hb with a ha,
---      unfold set.range,
---      unfold has_mem.mem set.mem set_of,
-      use -a,
-      rw [f.map_neg, ha],
-    end
-  }
-}
+lemma comap_comp {G₁ : Type*} [add_group G₁] {G₂ : Type*} [add_group G₂]
+  {G₃ : Type*} [add_group G₃] (f : add_group_hom G₁ G₂) (g : add_group_hom G₂ G₃)
+  (H : add_subgroup G₃) :
+  add_group_hom.comap (g.comp f) H = add_group_hom.comap f (add_group_hom.comap g H)
+  := rfl
+
+-- this works:
+--def ker (f : add_monoid_hom G H) : add_submonoid G := f.comap ⊥
+
+def ker (f : add_group_hom G H) : add_subgroup G := add_group_hom.comap f ⊥
+
+lemma mem_ker (f : add_group_hom G H) (g : G) : g ∈ ker f ↔ f g = 0 :=
+set.mem_singleton_iff
+
+-- but f.comap ⊥ doesn't :-/
+
+def range (f : add_group_hom G H) : add_subgroup H := add_group_hom.map f ⊤ 
 
 end add_group_hom
 
+/- quotients -/
+
 namespace add_subgroup
 
+variables {A : Type*} [add_comm_group A]
+
+@[derive add_comm_group]
+def quotient (B : add_subgroup A) := quotient_add_group.quotient B.carrier
+
+variable (B : add_subgroup A)
+
+def quotient.mk : add_group_hom A B.quotient :=
+{ to_fun := quotient.mk',
+  map_zero' := rfl,
+  map_add' := λ _ _, rfl }
+
+lemma quotient.ker_mk : add_group_hom.ker (quotient.mk B) = B :=
+begin
+  rw ←ext_iff,
+  intro g,
+  rw add_group_hom.mem_ker,
+  sorry -- should be easy
+end
+
 def quotient.lift {A : Type*} [add_comm_group A] {C : Type*} [add_comm_group C]
-  (f : A →+ C) (B : add_subgroup A) (hB : B ⊆ add_group_hom.ker f) :
+  (f : add_group_hom A C) (B : add_subgroup A) (hB : B ≤ add_group_hom.ker f) :
 B.quotient →+ C :=
 { to_fun := λ q, q.lift_on' ⇑f begin
     intros a₁ a₂ h,
     change (-a₁) + a₂ ∈ B at h,
-    have h2 : f (-a₁ + a₂) = 0 := hB h,
+    have h2 : f (-a₁ + a₂) = 0 := (f.mem_ker _).1 (hB h),
     rwa [f.map_add, f.map_neg, neg_add_eq_iff_eq_add,
       add_zero, eq_comm] at h2,
   end,
@@ -99,7 +129,16 @@ B.quotient →+ C :=
 
 
 def quotient.map {A₁ : Type*} [add_comm_group A₁] {A₂ : Type*} [add_comm_group A₂]
-  (B₁ : add_subgroup A₁) (B₂ : add_subgroup A₂) (f : A₁ →+ A₂) (hf : B₁.map f ⊆ B₂) :
-quotient B₁ → quotient B₂ := sorry -- use quotient.lift on induced group hom A₁ → A₂/B₂
+  (B₁ : add_subgroup A₁) (B₂ : add_subgroup A₂) (f : add_group_hom A₁ A₂)
+  (hf : B₁ ≤ add_group_hom.comap f B₂) :
+  add_group_hom (quotient B₁) (quotient B₂) :=
+{ to_fun := quotient.lift ((quotient.mk B₂).comp f) B₁ $ le_trans hf begin
+  unfold add_group_hom.ker,
+  rw add_group_hom.comap_comp,
+  sorry
+end,
+  map_zero' := sorry,
+  map_add' := sorry }
+-- sorry -- use quotient.lift on induced group hom A₁ → A₂/B₂
 
 end add_subgroup
--- src/add_subgroup/basic.lean
+++ src/add_subgroup/basic.lean
@@ -4,21 +4,75 @@ structure add_subgroup (G : Type*) [add_group G] :=
 (carrier : set G)
 (is_add_subgroup : is_add_subgroup carrier) 
 
-instance foo (G : Type*) [add_group G] (H : add_subgroup G) :
-  is_add_subgroup H.carrier := H.is_add_subgroup
-
-instance (G : Type*) [add_group G] : has_le (add_subgroup G) :=
-⟨λ H K, H.carrier ⊆ K.carrier⟩
-
-instance (G : Type*) [add_group G] : has_subset (add_subgroup G) :=
-⟨λ H K, H.carrier ⊆ K.carrier⟩
-
-instance (G : Type*) [add_group G] : has_mem G (add_subgroup G) :=
-⟨λ g H, g ∈ H.carrier⟩
-
 namespace add_subgroup
 
-variables {G : Type*} [add_group G] (H : add_subgroup G)
+variables (G : Type*) [add_group G]
+
+instance foo (H : add_subgroup G) :
+  _root_.is_add_subgroup H.carrier := H.is_add_subgroup
+
+instance : has_coe (add_subgroup G) (set G) :=
+⟨add_subgroup.carrier⟩
+
+instance : has_mem G (add_subgroup G) :=
+⟨λ g H, g ∈ (H : set G)⟩
+
+theorem ext' {G : Type*} [add_group G] {H K : add_subgroup G} (h : (H : set G) = K) : H = K :=
+by cases H; cases K; congr'
+
+@[ext] theorem ext_iff {G : Type*} [add_group G] {H K : add_subgroup G} :
+  (∀ g : G, g ∈ H ↔ g ∈ K) ↔ H = K :=
+begin
+  split,
+  { intro h,
+    apply ext',
+    exact set.ext h
+  },
+  intro h, rw h, intro g, refl,
+end
+
+
+
+/- Lattice stuff -/
+
+instance : partial_order (add_subgroup G) :=
+partial_order.lift (coe : add_subgroup G → set G) (λ a b, ext') (by apply_instance)
+
+open set
+
+universe u
+
+def bot {G : Type u} [add_group G] : add_subgroup G :=
+{ carrier := {0},
+  is_add_subgroup := { zero_mem := mem_singleton _,
+  add_mem := λ a b ha hb, begin
+    rw mem_singleton_iff at ha hb,
+    rw [ha, hb, add_zero, mem_singleton_iff],
+  end,
+  neg_mem := λ a ha, begin
+    rw mem_singleton_iff at ha,
+    rw [ha, neg_zero, mem_singleton_iff],
+  end } }
+
+instance : lattice.has_bot (add_subgroup G) := ⟨bot⟩
+
+def top {G : Type u} [add_group G] : add_subgroup G :=
+{ carrier := univ,
+  is_add_subgroup := { 
+    zero_mem := mem_univ _,
+    add_mem := λ _ _ _ _, mem_univ _,
+    neg_mem := λ _ _, mem_univ _ } 
+}
+
+instance : lattice.has_top (add_subgroup G) := ⟨top⟩
+
+instance : has_subset (add_subgroup G) := ⟨has_le.le⟩
+
+
+
+variable {G} 
+
+variables (H : add_subgroup G)
 
 def zero_mem : (0 : G) ∈ H :=
 begin
@@ -44,16 +98,10 @@ instance : has_coe_to_sort (add_subgroup G) :=
   coe := λ H, {g : G // g ∈ H.carrier }
 }
 
-def map {G₁ : Type*} [add_group G₁] {G₂ : Type*} [add_group G₂]
-  (H : add_subgroup G₁) (f : G₁ →+ G₂) : add_subgroup G₂ :=
-{ carrier := (f : G₁ → G₂) '' H.carrier,
-  is_add_subgroup := { 
-    zero_mem := ⟨0, H.zero_mem, f.map_zero⟩,
-    add_mem := begin rintros _ _ ⟨c, hc, rfl⟩ ⟨d, hd, rfl⟩,
-      exact ⟨c + d, H.add_mem hc hd, f.map_add c d⟩ end,
-    neg_mem := begin rintros _ ⟨c, hc, rfl⟩, exact ⟨-c, H.neg_mem hc, f.map_neg c⟩ end
-  } 
-}
+
+
+-- could prove that these are Galois connections and that add_subgroup G
+-- is a complete lattice.
 
 instance (H : add_subgroup G) : add_group ↥H :=
 { add := λ h₁ h₂, ⟨h₁.1 + h₂.1, H.add_mem h₁.2 h₂.2⟩,
@@ -70,10 +118,5 @@ variables {A : Type*} [add_comm_group A]
 instance (B : add_subgroup A) : add_comm_group ↥B :=
 { add_comm := λ a b, subtype.ext.2 $ add_comm _ _,
   ..add_subgroup.add_group B}
-
-@[derive add_comm_group]
-def quotient (B : add_subgroup A) := quotient_add_group.quotient B.carrier
-
-
 
 end add_subgroup
