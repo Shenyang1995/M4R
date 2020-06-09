c53b64de6b7a87441b358119673adf005b433415
shenyang wu <shenyang.wu16@imperial.ac.uk>
2020-06-09 18:08:01 +0100

    H^0 added

--- src/cohomology.lean
+++ src/cohomology.lean
@@ -9,7 +9,7 @@ variables (n:ℕ )(G : Type*) [group G] (M : Type*) [add_comm_group M] [G_module
 
 def cocycle := (add_group_hom.ker (d (n+1) G M))
 
-def coboundary := add_group_hom.range (d n G M)
+def coboundary := add_group_hom.range (d n G M) 
 
 theorem cob_sub_of_coc: coboundary n G M ≤ cocycle n G M:=
 range_d_sub_ker_d _ G M
@@ -19,3 +19,20 @@ def cohomology : add_subquotient (cochain (n + 1) G M) :=
 top := cocycle n G M,
 incl := cob_sub_of_coc n G M}
 
+def h0 : add_subquotient (cochain 0 G M):=
+{bottom := ⊥ ,
+top := (add_group_hom.ker (d 0 G M)),
+incl := begin 
+show  ⊥ ⊆ add_group_hom.ker (d 0 G M),
+intro a,
+intro ha,
+rw add_subgroup.mem_coe, 
+have h1:a=0,
+exact ha,
+rw add_group_hom.mem_ker,
+rw h1,
+show d.to_fun 0=0,
+exact (d 0 G M).map_zero',
+end}
+
+--def H_n : add_subquotient (cochain n G M):= if n=0 then h0 G M else cohomology (n-1) G M
